ed798e6daebbb0eb2d4b3f088b4564779b5ede52
kh813 <10515511+kh813@users.noreply.github.com>
2025-01-11 18:05:44 +0900

    Allow overwrite

--- generate_exe.py
+++ generate_exe.py
@@ -4,7 +4,7 @@ exe,appファイルの作成
 import subprocess
 
 def Generate_exe():
-    result = subprocess.run("pyinstaller pdf_gui.py --onefile --noconsole", shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+    result = subprocess.run("pyinstaller pdf_gui.py --onefile --noconsole --noconfirm", shell=True, stdout=subprocess.PIPE, universal_newlines=True)
 
 if __name__=="__main__":
     Generate_exe()
